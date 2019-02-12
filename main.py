f72230d1746cad2d129eab66b32fff5290e89d8d
Shadowian <47497790+Shadowian@users.noreply.github.com>
2019-02-12 12:07:29 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,51 +1,23 @@
 import subprocess
+from datetime import datetime
 import platform
 
-# Checking for current OS
+#Checking for current OS
 osName = platform.system()
 
 
-def TimeConvert(minutes, hours):
-    # Convert hours to minutes and add to original minute count
-    total = minutes + (hours * 60)
-    # Convert to seconds from minutes
-    seconds = total * 60
-    return seconds
+#Grabbing minutes/hours till shutdown and printing
+sleepMinutes = input("In how many minutes do you want the PC to shutdown?: ")
+sleepHours = input("In how many hours do you want the PC to shutdown?: ")
+
+print("The PC will shutdown in " + str(sleepMinutes) + " minutes and " + str(sleepHours) + " hours.")
 
 
-def GetInput():
-
-    # Grabbing minutes/hours till shutdown and returning number
-    # Creates the Global variable rather than them being confined to
-    # the current function
-
-    global slp_min
-    global slp_hrs
-
-    # Error checking for input, Makes sure is actually a number.
-    try:
-        slp_hrs = int(input(
-            "In how many hours do you want the PC to shutdown?: "))
-        slp_min = int(input(
-            "In how many minutes do you want the PC to shutdown?: "))
-    # Exception that is thrown when entering non-integer value.
-    # Restarts getinput function rather than completely stopping.
-    except(ValueError):
-        print("This is not a number")
-        GetInput()
-
-
-GetInput()
-
-print(f"The pc will shutdown in {slp_hrs} hours and {slp_min} minutes")
-print(f"Shutdown in {TimeConvert(slp_min, slp_hrs)} seconds.")
-
-# Passing shutdown command to terminal. Will differ if it's Unix or Windows OS
-if osName == "Windozs":
-    subprocess.run(["shutdown", "/s", "/t", TimeConvert(slp_min, slp_hrs)])
-
-# idea: adding Time and Date here at which time it will shutdown
-# idea: maybe add wait command, so that the popup window wont show up
-
+#Passing shutdown command to terminal. Will differ if it's Unix or Windows OS
+if osName == "Windows":
+    timeToSeconds = sleepMinutes * 60 + sleepHours * 60 * 60
+    subprocess.run(["shutdown", "/s", "/t", timeToSeconds])
+    #idea: adding Time and Date here at which time it will shutdown
+    #idea: maybe add wait command, so that the popup window wont show up
 else:
-    print("Sorry i don't support other OS's yet.")
+    subprocess.run(["shutdown", sleepMinutes])
